06d769cf7569422b0e925aa8bd0d2dcd95359e72
radu-mocanu <radu.mocanu@uipath.com>
2025-05-27 19:24:39 +0300

    feat: hitl models support

--- samples/multi-agent/main.py
+++ samples/multi-agent/main.py
@@ -1,40 +1,45 @@
+import json
+
+from dotenv import load_dotenv
 from llama_index.core.agent.workflow import AgentWorkflow
 from llama_index.core.workflow import (
     Context,
     HumanResponseEvent,
-    InputRequiredEvent,
 )
 from llama_index.llms.openai import OpenAI
+
+from uipath_llamaindex.models import InvokeProcessEvent
+
+load_dotenv()
 
 llm = OpenAI(model="gpt-4o-mini")
 
 
-async def may_research_company(ctx: Context, company_name: str) -> bool:
-    """Find whether a company may be researched.
+async def may_research_company(ctx: Context, company_name: str) -> str:
+    """Find whether a company may be researcher.
     Args:
         ctx (Context): The context in which this function is called (autopopulated).
         company_name (str): Name of the company to be researched.
     Returns:
-        bool: True if the company can be researched, False otherwise.
+        str: company report
     """
-    print("Researching company...")
-
     # emit an event to the external stream to be captured
     ctx.write_event_to_stream(
-        InputRequiredEvent(
-            prefix=f"May I perform a research on company {company_name}? \n (yes/no)"
+        InvokeProcessEvent(
+            prefix="invoke langgraph researcher event",
+            name="my-first-uipath-agent",
+            # process_folder_path="(optional)<process-folder-path>",
+            input_arguments={
+                "topic": company_name,
+            },
         )
     )
 
     # wait until we see a HumanResponseEvent
-    response = await ctx.wait_for_event(HumanResponseEvent)
-    print("Received response:", response.response)
-
+    hitl_response = await ctx.wait_for_event(HumanResponseEvent)
+    feedback = json.loads(hitl_response.response)
     # act on the input from the event
-    if response.response.strip().lower() == "yes":
-        return True
-    else:
-        return False
+    return feedback["report"]
 
 
 workflow = AgentWorkflow.from_tools_or_functions(
